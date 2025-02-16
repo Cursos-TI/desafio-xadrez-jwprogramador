1d2cd1ac4af3ea9fd99247eccf5280ae3d32c40d
jwprogramador <josewilsonbto@gmail.com>
2025-02-16 13:02:02 +0000

    Codigo nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -12,6 +12,7 @@ int main() {
      int rainha = 8;
      int x = 0;
      int i;
+     
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
@@ -31,7 +32,6 @@ int main() {
     
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
     printf("Movimentação da Rainha: \n");
     for (i = 0; i < rainha; i++) {
         printf("Esquerda\n");
@@ -39,7 +39,19 @@ int main() {
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("Movimentação do cavalo: \n");
+    int cavalo = 2;
+
+    for (i = 2; i <= cavalo; i++){
+            int j;
+           for (j = 1; j <= i; j++)
+           {
+            printf("Baixo\n");
+           }
+            
+        printf("Esquerda\n");   
+    }
+
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
